15c055bad19f106625ed6832ee9e8b45c7635ac7
milocode-dev <radityaakmal277@gmail.com>
2026-07-06 17:28:57 +0700

    Setting route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Routing Halaman Utama CRUD
+Route::get('/book', [BookController::class, 'index'])->name('book.index');
+Route::get('/book/create', [BookController::class, 'create'])->name('create.book');
+Route::post('/book', [BookController::class, 'store'])->name('store.book');
+Route::get('/book/{id}/edit', [BookController::class, 'edit'])->name('edit.book');
+Route::put('/book/{id}', [BookController::class, 'update'])->name('update.book');
+Route::delete('/book/{id}', [BookController::class, 'delete'])->name('delete');
